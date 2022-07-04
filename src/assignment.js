ead814143846d99ba977964ccbdf2eeffc25f970
Adetuwo Ezekiel Mofiyinfoluwa <108675383+Zhikky@users.noreply.github.com>
2022-07-04 21:01:41 +0000

    I attempted the three challenges.
    Check challenges 1,2 and 3

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,18 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
+let arrayOfNumbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum=0
+    for (let sumOfNumbers of arrayOfNumbers){
+        sum= sum+ sumOfNumbers;
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +35,35 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
+//function countEvenNumbers(arrayOfNumbers) {
+  //  return even.length;
+//}
+//let newArray=[]
+//function countEvenNumbers(arrayOfNumbers) {
+  
+ // for(let i=0; i<=arrayOfNumbers.length; i++){
+    
+ //   let even= arrayOfNumbers[i]
+ //   if (even%2==0){
+ //   newArray.push(even)}
+ // }
+ //   return newArray.length;
+//}
+
+let newArray=[]
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    
+for(i=0; i<arrayOfNumbers.length; i++){
+  
+   arrayOfNumbers[i]
+  if(arrayOfNumbers[i]%2==0){
+    newArray.push(arrayOfNumbers[i])
+  }
+    } return newArray.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +80,14 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+  let newArray=[]
+  for (let tempCelcius of arrayOfNumbers){
+    let tempFahrenheit= (tempCelcius *1.8)+32
+    newArray.push(Math.trunc(tempFahrenheit))
+  } return newArray
+} 
+
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
